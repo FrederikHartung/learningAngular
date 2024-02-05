7446613cfb4f98c53ddbf8f1aa42bc7111a272b2
Frederik Hartung <109949516+frederikHartung-extern@users.noreply.github.com>
2024-02-05 09:21:24 +0100

    added alerts

---
+++ src/app/successAlert/successAlert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-success-alert',
+    templateUrl: './successAlert.component.html',
+    styles: ['h3 { color: green; }']
+})
+export class SuccessAlertComponent {
+
+}
