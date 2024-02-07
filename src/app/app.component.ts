6b0c94a311abed325261ab604338278e4acf6771
Frederik Hartung <109949516+frederikHartung-extern@users.noreply.github.com>
2024-02-07 22:30:11 +0100

    added username input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,26 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  userName = 'DummyName';
+  allowReset = this.userName === '' ? false : true;
+  title = 'my-second-app'
+
+  onClick() {
+    this.userName = '';
+    this.allowReset = false;
+    console.log('Button was clicked');
+  }
+
+  onInput(event: Event) {
+    let newValue = (<HTMLInputElement>event.target).value;
+    this.userName = newValue;
+    
+    if(newValue === '') {
+      this.allowReset = false;
+    } else {
+      this.allowReset = true;
+    }
+
+    console.log('is allowed to reset: ' + this.allowReset);
+  }
 }
