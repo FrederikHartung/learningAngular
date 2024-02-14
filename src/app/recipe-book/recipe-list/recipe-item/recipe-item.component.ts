cb423d2ac37032bbbaffbaf62bf38a5b201dadef
FrederikHartung <90627809+FrederikHartung@users.noreply.github.com>
2024-02-14 09:06:07 +0100

    added recipe to recipe-item

--- src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-item.component.css'
 })
 export class RecipeItemComponent {
+  recipe: Recipe;
 
+  constructor(recipe: Recipe){
+    this.recipe = recipe;
+  }
+
+  getRecipe(){
+    return this.recipe;
+  }
 }
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-  recipes = [];
+  recipes: Recipe[] = [new Recipe('A Test Recipe', 'This is simply a test', 'https://www.simplyrecipes.com/wp-content/uploads/2019/03/shakshuka-horiz-a-1600.jpg')];
 
 }
