6c15bb108cc651b355af1dde8a14fa4a0112499b
FrederikHartung <90627809+FrederikHartung@users.noreply.github.com>
2024-02-16 10:26:42 +0100

    added dynamic list item display with image

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -16,12 +16,17 @@ export class RecipeListComponent {
     new Recipe(
       'Test Title 2',
       'Test Description 2',
-      'https://www.simplyrecipes.com/wp-content/uploads/2019/03/shakshuka-horiz-a-1600.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
     ),
     new Recipe(
       'Test Title 3',
       'Test Description 3',
-      'https://www.simplyrecipes.com/wp-content/uploads/2019/03/shakshuka-horiz-a-1600.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+    ),
+    new Recipe(
+      'Saskias Guoza',
+      'Her favorite food',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Japanese_pan_fried_gyoza.jpg/1599px-Japanese_pan_fried_gyoza.jpg?20170712141633'
     )
   ];
 }
